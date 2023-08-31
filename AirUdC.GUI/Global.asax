723bbab6cbda53305b593df60af14d18717eb66a
sannt <sannt@DANNYS>
2023-08-31 15:03:26 -0500

    Agregar archivos de proyecto.

---
+++ AirUdC.GUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AirUdC.GUI.MvcApplication" Language="C#" %>
---
+++ AirUdC.GUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AirUdC.GUI.Startup))]
+namespace AirUdC.GUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
