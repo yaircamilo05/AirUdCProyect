d71d152fdce4bce60f2e04eb6327f6a4431242fc
julianagomez21 <jgomezgal25@gmail.com>
2023-12-13 15:35:32 -0500

    Extrayendo DEV

--- AirUdC.GUI/Global.asax.cs
+++ AirUdC.GUI/Global.asax.cs
@@ -1,7 +1,16 @@
+<<<<<<< Updated upstream
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+=======
+using AirbnbUdC.Application.Contracts.Contracts.Parameters;
+using AirbnbUdC.Application.Implementation.Implementation.Parameters;
+using AirbnbUdC.Repository.Contracts.Contrats.Parameters;
+using AirbnbUdC.Repository.Implementation.Implementations.Parameters;
+using Autofac;
+using Autofac.Integration.Mvc;
+>>>>>>> Stashed changes
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
