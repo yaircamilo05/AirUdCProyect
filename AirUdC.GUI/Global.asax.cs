4afbdb73c983269d34bab17998dfdcb3b871ed61
julianagomez21 <jgomezgal25@gmail.com>
2023-12-13 15:36:52 -0500

    Solucion de merge

--- AirUdC.GUI/Global.asax.cs
+++ AirUdC.GUI/Global.asax.cs
@@ -1,16 +1,13 @@
-<<<<<<< Updated upstream
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-=======
 using AirbnbUdC.Application.Contracts.Contracts.Parameters;
 using AirbnbUdC.Application.Implementation.Implementation.Parameters;
 using AirbnbUdC.Repository.Contracts.Contrats.Parameters;
 using AirbnbUdC.Repository.Implementation.Implementations.Parameters;
 using Autofac;
 using Autofac.Integration.Mvc;
->>>>>>> Stashed changes
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
